10ee42f150644dfa9c2f9b55612004e32ae4531f
abdusalam <2698788007@qq.com>
2022-05-05 22:29:47 +0800

    第二次提交

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rj8rqu0=(_#)dtlxvf_n3ev*re(-+4qi@7s$l&@m_6qiuz4pfk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
